adb572d747f5e3ff82400f6bebaf6e46eb9df9e2
Anusha <anushat6633@gmail.com>
2025-06-13 10:42:30 +0530

    Update preprocessing.py

--- preprocessing.py
+++ preprocessing.py
@@ -1,1 +1,12 @@
+import numpy as np
+from scipy.signal import butter, filtfilt
 
+def bandpass_filter(signal, lowcut=0.5, highcut=40.0, fs=250, order=2):
+    nyq = 0.5 * fs
+    low = lowcut / nyq
+    high = highcut / nyq
+    b, a = butter(order, [low, high], btype='band')
+    return filtfilt(b, a, signal)
+
+def preprocess_ecg(raw_ecg):
+    return bandpass_filter(raw_ecg)
